4634ea0ee64fd43e4de152a642fa5ded934af1e9
Kurmuro <55317081+Kurmuro@users.noreply.github.com>
2019-12-28 02:15:19 +0100

    geht wieder...

--- app/src/main/java/de/wassersportclub/wcg/TeilnehmerErstellTabelle.java
+++ app/src/main/java/de/wassersportclub/wcg/TeilnehmerErstellTabelle.java
@@ -1,23 +1,31 @@
 package de.wassersportclub.wcg;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class TeilnehmerErstellTabelle extends AppCompatActivity {
 
-    TextView willkommenstextTV, emailView, vornameView, nachnameView, bootstyp, yardstick;
+    TextView willkommenstextTV, emailView, vornameView, nachnameView, bootstypView, yardstickView;
     Button logoutBTN, finishBTN;
 
     FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +39,8 @@ public class TeilnehmerErstellTabelle extends AppCompatActivity {
         emailView = findViewById(R.id.TABLEemail);
         vornameView = findViewById(R.id.TABLEvorname);
         nachnameView = findViewById(R.id.TABLEnachname);
-        bootstyp = findViewById(R.id.TABLEbootstyp);
-        yardstick = findViewById(R.id.TABLEyardstick);
+        bootstypView = findViewById(R.id.TABLEbootstyp);
+        yardstickView = findViewById(R.id.TABLEyardstick);
 
         //Startet den Listener für alle buttons
         doListen();
@@ -49,9 +57,49 @@ public class TeilnehmerErstellTabelle extends AppCompatActivity {
         finishBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+                final DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
 
-                    mDatabase.child("users").child("hhjghk").setValue("wert");
+
+                final String vorname = vornameView.getText().toString();
+                final String nachname = nachnameView.getText().toString();
+                final String bootstyp = bootstypView.getText().toString();
+                final String email = emailView.getText().toString();
+                int yardstick = 0;
+
+                if(!yardstickView.getText().toString().isEmpty()) {
+                    yardstick = Integer.parseInt(yardstickView.getText().toString());
+                }
+
+
+                if(email != null && !email.isEmpty() && vorname != null && !vorname.isEmpty() && nachname != null && !nachname.isEmpty() &&
+                        bootstyp != null && !bootstyp.isEmpty() && yardstick >= 1 && yardstick <= 9999 ) {
+
+                    mAuth.createUserWithEmailAndPassword(email, "123456")
+                            .addOnCompleteListener(TeilnehmerErstellTabelle.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        FirebaseUser user = task.getResult().getUser();
+                                        mDatabase.child(user.getUid()).child("Email").setValue(email);
+                                        mDatabase.child(user.getUid()).child("Vorname").setValue(vorname);
+                                        mDatabase.child(user.getUid()).child("Nachname").setValue(nachname);
+                                        mDatabase.child(user.getUid()).child("Bootstyp").setValue(bootstyp);
+                                        mDatabase.child(user.getUid()).child("Yardstick").setValue(Integer.parseInt(yardstickView.getText().toString()));
+                                        Intent intent = getIntent();
+                                        finish();
+                                        startActivity(intent);
+                                        Toast.makeText(TeilnehmerErstellTabelle.this, "Benutzer hinzugefügt", Toast.LENGTH_SHORT).show();
+
+                                    } else {
+                                        // Registrieren schief gegangen
+                                        Log.w("test", "createUserWithEmail:failure", task.getException());
+                                        Toast.makeText(TeilnehmerErstellTabelle.this, "Hinzufügen hat nicht geklappt", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }else{
+                    Toast.makeText(TeilnehmerErstellTabelle.this, "Fehler: Alles richtig eingegeben?", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
--- app/src/main/java/de/wassersportclub/wcg/TeilnehmerVerwaltung.java
+++ app/src/main/java/de/wassersportclub/wcg/TeilnehmerVerwaltung.java
@@ -1,14 +1,18 @@
 package de.wassersportclub.wcg;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+
 
 public class TeilnehmerVerwaltung extends AppCompatActivity {
 
@@ -60,7 +64,32 @@ public class TeilnehmerVerwaltung extends AppCompatActivity {
         deleteBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                /*if(data.getCount() == 0){
+                    Toast.makeText(TeilnehmerVerwaltung.this, "Keine Teilnehmer Vorhanden", Toast.LENGTH_LONG).show();
+                    return;
+                }
 
+                List<String> users = new ArrayList<String>();
+                String[] userlist = new String[users.size()];
+
+                List<Integer> numbers = new ArrayList<Integer>();
+
+                while(data.moveToNext()){
+                    StringBuffer buffer = new StringBuffer();
+                    numbers.add(data.getInt(0));
+                    buffer.append("Vorname: " + data.getString(1) + "\n");
+                    buffer.append("Nachname: " + data.getString(2) + "\n");
+                    buffer.append("Boottyp: " + data.getString(3) + "\n");
+                    buffer.append("Yardstick: " + data.getInt(4) + "\n");
+
+                    users.add( buffer.toString());
+
+                }
+
+                userlist = users.toArray(userlist);
+                displayDeleteView("Teilnehmer auswählen:", userlist);
+
+                 */
             }
         });
         resetBTN.setOnClickListener(new View.OnClickListener() {
@@ -76,5 +105,24 @@ public class TeilnehmerVerwaltung extends AppCompatActivity {
         mAuth.signOut();
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void displayDeleteView(String title, String[] userlist){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setItems(userlist, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                int deleteRow = 1;
+                if(deleteRow > 0){
+                    Toast.makeText(TeilnehmerVerwaltung.this, "Erfolgreich gelöscht", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(TeilnehmerVerwaltung.this, "Irgendwas ist schief gegangen", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
